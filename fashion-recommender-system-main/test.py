3b06a320df6a1a98c43f1ec0e33a0818ef3a3f2e
JayaniMalsha <150431769+JayaniMalsha@users.noreply.github.com>
2024-03-13 12:17:55 +0530

    Create fashion recommendation
    use deep learning

---
+++ fashion-recommender-system-main/test.py
@@ -0,0 +1,20 @@
+import pickle
+import tensorflow
+import numpy as np
+from numpy.linalg import norm
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.layers import GlobalMaxPooling2D
+from tensorflow.keras.applications.resnet50 import ResNet50,preprocess_input
+from sklearn.neighbors import NearestNeighbors
+import cv2
+
+feature_list = np.array(pickle.load(open('embeddings.pkl','rb')))
+filenames = pickle.load(open('filenames.pkl','rb'))
+
+model = ResNet50(weights='imagenet',include_top=False,input_shape=(224,224,3))
+model.trainable = False
+
+model = tensorflow.keras.Sequential([
+    model,
+    GlobalMaxPooling2D()
+])
