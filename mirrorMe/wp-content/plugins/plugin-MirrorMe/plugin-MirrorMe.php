3ac7777e08ffc75eac20beb8436ef33dd672d530
AmayaDes <w1998759@my.westminster.ac.uk>
2024-03-24 18:13:54 +0530

    plugin

--- mirrorMe/wp-content/plugins/plugin-MirrorMe/plugin-MirrorMe.php
+++ mirrorMe/wp-content/plugins/plugin-MirrorMe/plugin-MirrorMe.php
@@ -6,16 +6,18 @@
  *
  * @woocommerce-plugin
  * Plugin Name:       MirrorMe
- * Plugin URI:        https://example.com/plugin-name
+ * Plugin URI:        https://amayades.github.io/Html/
  * Description:       Virtual try-on 
- * Version:           1.0.0
+ * Version:           0.1.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            SDGP CS-114
  * Author URI:        https://example.com
+ * 
+ * Woo: 12345:342928dfsfhsf8429842374wdf4234sfd
  */
 
- // Test to see if WooCommerce is active (including network activated).
+// Test to see if WooCommerce is active (including network activated).
 $plugin_path = trailingslashit( WP_PLUGIN_DIR ) . 'woocommerce/woocommerce.php';
 
 if (
@@ -24,7 +26,29 @@ if (
 ) {
 
     // Schedule the event on plugin activation
-    register_activation_hook( __FILE__, 'schedule_weekly_apparel_check' );
+    register_activation_hook( __FILE__, 'mirror_me_activate' );
+    register_deactivation_hook( __FILE__, 'mirror_me_deactivate' );
+    register_uninstall_hook( __FILE__, 'mirror_me_uninstall' );
+
+    function mirror_me_activate() {
+        // Activation tasks can be added here
+        // For example, you may want to schedule the event on plugin activation
+        schedule_weekly_apparel_check();
+    }
+
+    function mirror_me_deactivate() {
+        // Deactivation tasks can be added here
+        // For example, you may want to clear the scheduled event on plugin deactivation
+        wp_clear_scheduled_hook( 'weekly_apparel_check' );
+    }
+
+    // Define the callback function for uninstallation
+    function mirror_me_uninstall() {
+    // Remove scheduled event on plugin uninstallation
+    wp_clear_scheduled_hook( 'weekly_apparel_check' );
+
+    
+
 
     function schedule_weekly_apparel_check() {
         // Schedule the event to run weekly
@@ -60,29 +84,30 @@ if (
             wp_reset_postdata();
         }
 
-    // Output result
-    //add code here to send the images to model to create 3D models
-    
-}
-add_action( 'woocommerce_single_product_summary', 'add_custom_button_to_product_page', 25 );
+        // Output result
+        //add database
 
-function add_custom_button_to_product_page(){
-    // Output your custom button HTML with a link
-    echo '<a href="https://amayades.github.io/Html/" class="custom-button" target="_blank">Try-On</a>';
-    
-    // You can style the button using CSS
-    ?>
-    <style>
-    .custom-button {
-        position: absolute;
-        top: 20px;
-        right: 20px;
-        background-color: #007bff;
-        color: #fff;
-        padding: 10px 20px;
-        text-decoration: none;
     }
-    </style>
-    <?php
-}
+
+    add_action( 'woocommerce_single_product_summary', 'add_custom_button_to_product_page', 25 );
+
+    function add_custom_button_to_product_page(){
+        // Output your custom button HTML with a link
+        echo '<a href="https://amayades.github.io/Html/" class="custom-button" target="_blank">Try-On</a>';
+        
+        // You can style the button using CSS
+        ?>
+        <style>
+        .custom-button {
+            position: absolute;
+            top: 20px;
+            right: 20px;
+            background-color: #007bff;
+            color: #fff;
+            padding: 10px 20px;
+            text-decoration: none;
+        }
+        </style>
+        <?php
+    }
 }
